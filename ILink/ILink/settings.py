817bec69fa52f309a658c293f8d7cdefdeb3f848
Herman <officialzhanghang@gmail.com>
2016-03-31 10:30:06 +0800

    Add register bk_img

--- ILink/ILink/settings.py
+++ ILink/ILink/settings.py
@@ -124,6 +124,6 @@ STATIC_ROOT = os.path.join(os.path.dirname(__file__),'static')
 STATICFILES_DIRS = (
     ('css',os.path.join(STATIC_ROOT,'css').replace('\\','/') ),  
     ('js',os.path.join(STATIC_ROOT,'js').replace('\\','/') ),
-    ('images',os.path.join(STATIC_ROOT,'images').replace('\\','/') ),
+    ('img',os.path.join(STATIC_ROOT,'img').replace('\\','/') ),
     ('upload',os.path.join(STATIC_ROOT,'upload').replace('\\','/') ),
 )
