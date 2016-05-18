2ec61ef4f6ad4b395ead71547e26397184e63265
Herman <officialzhanghang@gmail.com>
2016-05-18 23:16:05 +0800

    create proj alert

--- ILink/projects/views.py
+++ ILink/projects/views.py
@@ -8,6 +8,8 @@ def index(request) :
 	return render(request , 'projects/myProjects.html')
 
 def myProjects(request) :
+	press_create = False
+	create_project_succeed = False
 	# import pdb
 	# pdb.set_trace()
 	# validate user session
@@ -24,6 +26,8 @@ def myProjects(request) :
 	return render(request , 'projects/myProjects.html',{
 		'is_login_success' : True,
 		'user_session' : account,
+		'press_create' : False,
+		'create_project_succeed' : False,
 		})
 
 
@@ -43,6 +47,8 @@ def projectDetail(request) :
 		})
 
 def projectQuickCreate(request):
+	press_create = False
+	create_project_succeed = False
 	# validate user session
 	try:
 		account = Account.objects.get(_account_name = request.session['account_name'])
@@ -53,17 +59,27 @@ def projectQuickCreate(request):
 			'is_first_time_to_this_page' : False,
 			})
 
+	
 	# check user input
 	try:
 		proj_name = request.POST['proj-name']
 		proj_description = request.POST['description']
-
+		
 		account.project_set.create(project_name = proj_name , project_description = proj_description , 
 			project_owner = account,project_start_date = datetime.now(), project_expire_date = (datetime.now() + timedelta(days = 30)))
+
+		
 	except Exception, e:
-		raise
+	
+		return render(request , 'projects/myProjects.html' , {
+			'is_login_success' : True,
+			'press_create' : True,
+			'create_project_succeed' : False,
+			})
 
 	return render(request , 'projects/projectDetail.html',{
 		'is_login_success' : True,
 		'user_session' : account,
+		'press_create' : True,
+		'create_project_succeed' : True,
 		})
