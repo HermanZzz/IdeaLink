4747ab6112549b44dd0f516dff6f5e851f8da719
Herman <officialzhanghang@gmail.com>
2016-06-21 16:34:33 +0800

    url refactor

--- ILink/ILink/urls.py
+++ ILink/ILink/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^accounts/' , include('accounts.urls', namespace='accoutns')),
+    url(r'^accounts/' , include('accounts.urls', namespace='accounts')),
     url(r'^projects/' , include('projects.urls', namespace='projects'))
 ]
